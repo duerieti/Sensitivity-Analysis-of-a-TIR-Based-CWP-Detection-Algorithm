882724893c8c3d04789c6f83befe8c92a4e6964f
Etienne Dürig <duerieti@login02.cm.cluster>
2026-04-13 16:21:35 +0200

    started memory testing

---
+++ code/memory_test/new_code/Slurm-295823/slurm_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=memory_test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=04:00:00
+#SBATCH --partition=earth-3
+#SBATCH --mem=5G
+
+module load USS/2022
+module load gcc/9.4.0-pe5.34
+module load lsfm-init-miniconda/1.0.0
+
+conda activate R_env
+
+Rscript function_reworked.R
+
+
