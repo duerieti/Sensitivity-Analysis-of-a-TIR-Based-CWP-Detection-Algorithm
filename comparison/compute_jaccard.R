c413d92ae662a76f406019272e64ca204446f246
Etienne <duerieti@students.zhaw.ch>
2026-06-18 14:04:19 +0200

    report state before major restructuring

--- comparison/compute_jaccard.R
+++ comparison/compute_jaccard.R
@@ -22,9 +22,21 @@ current_combi_2 <- sf::read_sf("./param_combi_2/current/final_polys.shp")
 new_combi_3     <- sf::read_sf("./param_combi_3/new/final_polys.shp")
 current_combi_3 <- sf::read_sf("./param_combi_3/current/final_polys.shp")
 
+
+
+
+v3 <- sf::read_sf("./param_combi_1/v3_12_core/final_polys.shp")
+
+
 # compute the Jaccard similarity (intersection over union of the combined
 # patch areas) between the new and current implementation, for each
 # parameter combination, to assess output equivalence
 jaccard_similarity(new_combi_1, current_combi_1)
 jaccard_similarity(new_combi_2, current_combi_2)
 jaccard_similarity(new_combi_3, current_combi_3)
+
+
+
+jaccard_similarity(v3,current_combi_1)
+jaccard_similarity(v3,current_combi_2)
+jaccard_similarity(v3,current_combi_3)
